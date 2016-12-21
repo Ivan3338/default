d4fcbca5ebd04d6e8216187fe850a81b1892b8a4
Ivan3338 <berserk3338@gmail.com>
2016-12-21 20:08:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sudo apt update;
 sudo apt install apache2 php7.0 apache2-mod-php7.0 nginx libapache2-mod-rpaf;
-sudo apt install mpstat iostat;
+sudo apt install systat;
 sudo service apache2 stop;
 sudo service nginx stop;
 sudo rmdir /etc/apache2 /etc/nginx;
