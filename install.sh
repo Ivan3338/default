da7de9b829dfa3495c9e5d4b429f65d261e8288f
Ivan3338 <berserk3338@gmail.com>
2016-12-21 19:27:57 +0300

    2

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 sudo apt install apache2 php7.0 apache2-mod-php7.0 nginx libapache2-mod-rpaf;
+sudo service apache2 stop;
+sudo service nginx stop;
+sudo rmdir /etc/apache2 /etc/nginx;
 sudo cp -r ./* /var/www/html;
 sudo cp -r ./files/apache2 /etc;
 sudo cp -r ./files/nginx /etc;
-
+sudo service apache2 start;
+sudo service nginx start;
 
