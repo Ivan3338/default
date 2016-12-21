ba9e5f79dc56d037ee5c340702370b045feea97a
Ivan3338 <berserk3338@gmail.com>
2016-12-21 19:35:09 +0300

    11

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 sudo apt install apache2 php7.0 apache2-mod-php7.0 nginx libapache2-mod-rpaf;
+sudo apt install mpstat iostat;
 sudo service apache2 stop;
 sudo service nginx stop;
 sudo rmdir /etc/apache2 /etc/nginx;
 sudo cp -r ./* /var/www/html;
 sudo cp -r ./files/apache2 /etc;
 sudo cp -r ./files/nginx /etc;
+sudo chmod -R 666 /var/www/html/*;
+sudo chmod 777 /var/www/html scriptcrontoptalkers.sh scriptinfo.sh scripttoptalkers.sh;
 sudo service apache2 start;
 sudo service nginx start;
 
