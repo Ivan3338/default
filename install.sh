44a90bf939d0a005b9f5ca9cb68e4cbc8643447f
Ivan3338 <berserk3338@gmail.com>
2016-12-21 20:24:44 +0300

    123

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install apache2 php7.0 apache2-mod-php7.0 nginx libapache2-mod-rpaf;
-sudo apt install mpstat iostat;
+sudo apt install sysstat;
 sudo service apache2 stop;
 sudo service nginx stop;
 sudo rmdir /etc/apache2 /etc/nginx;
